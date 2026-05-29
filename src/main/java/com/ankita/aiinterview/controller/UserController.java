7b066be16105bd11db0ab26221e5f5dd9db805b0
Adev-java <ankitasharma15161@gmail.com>
2026-05-29 14:46:07 +0530

    Added userController file

---
+++ src/main/java/com/ankita/aiinterview/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.ankita.aiinterview.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ankita.aiinterview.entity.User;
+import com.ankita.aiinterview.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
